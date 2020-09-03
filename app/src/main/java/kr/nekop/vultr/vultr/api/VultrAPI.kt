57b15dfea832fa42aa11bd5081bfe525f48011a9
KuroNeko <kuroneko9505@gmail.com>
2020-09-03 09:45:27 +0900

    first commit

---
+++ app/src/main/java/kr/nekop/vultr/vultr/api/VultrAPI.kt
@@ -0,0 +1,12 @@
+package kr.nekop.vultr.vultr.api
+
+import kr.nekop.vultr.vultr.api.util.APIRequest
+
+class VultrAPI(
+    val apiKey: String,
+    val requestor: APIRequest = APIRequest(apiKey)
+) {
+    val account: Account = Account(requestor)
+    val application: Application = Application(requestor)
+
+}
